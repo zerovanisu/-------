b11756f82c5e9c37a98bc030616116e9fd3cee68
zerovanisu <67454773+zerovanisu@users.noreply.github.com>
2020-07-29 02:02:41 +0900

    abc
    def

---
+++ Assets/Script/Title.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    
+    public void gamestart()
+    {
+        SceneManager.LoadScene("Chapter");   
+    }
+
+    public void EndGame() // ゲームを終了する
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+}
---
+++ Beauty and the Beast Battle History1.1/Assets/Script/Fade_Object3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade_Object3 : MonoBehaviour
+{
+    GameObject ManageObject3;
+    FadeInOut fadeManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SceneFadeManagerがアタッチされているオブジェクトを取得
+        ManageObject3 = GameObject.Find("ManageObject3");
+        //オブジェクトの中のSceneFadeManagerを取得
+        fadeManager = ManageObject3.GetComponent<FadeInOut>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Scenecahnge()
+    {
+        //SceneFadeManagerの中のフェードアウト開始関数を呼び出し
+        fadeManager.fadeOutStart(0, 0, 0, 0, "Scene");
+    }
+}
