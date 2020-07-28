c13ad9553a19b14987b5295907aa6a653271876e
zerovanisu <67454773+zerovanisu@users.noreply.github.com>
2020-07-29 00:55:38 +0900

    再うp
    そろそろオナシャス...

---
+++ Assets/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject Belle;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 BellePos = Belle.transform.position;
+
+        //カメラとプレイヤーの位置を同じにする
+        transform.position = new Vector3(BellePos.x, -1.7f, -4);
+    }
+
+}
