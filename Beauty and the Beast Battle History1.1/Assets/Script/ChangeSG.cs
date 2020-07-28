42db8bfb8436b2c65a8337cf549600f9e65bdda3
zerovanisu <67454773+zerovanisu@users.noreply.github.com>
2020-07-29 00:53:55 +0900

    再うp
    今度こそオナシャス！

---
+++ Beauty and the Beast Battle History1.1/Assets/Script/ChangeSG.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSG : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject changebutton1;
+    [SerializeField]
+    private GameObject changebutton2;
+    [SerializeField]
+    private GameObject changeC1;
+    [SerializeField]
+    private GameObject changeC2;
+
+    private void Update()
+    {
+        if(Input.GetKey("r"))
+        {
+            changeSt1();
+            
+        }
+        else
+        {
+            changeSt2();
+        }
+    }
+
+    public void changeSt1()
+    {
+        changebutton1.SetActive(false);
+        changebutton2.SetActive(true);
+        changeC1.SetActive(true);
+        changeC2.SetActive(false);
+    }
+
+    public void changeSt2()
+    {
+        changebutton1.SetActive(true);
+        changebutton2.SetActive(false);
+        changeC1.SetActive(false);
+        changeC2.SetActive(true);
+    }
+
+    //IEnumerable change()
+    //{
+    //    changeSt1();
+    //    yield return null;
+    //    changeSt2();
+    //}
+}
---
+++ Beauty and the Beast Battle History1.1/Assets/Script/PauseScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+
+    //　アイテムメニューを開くボタン
+    [SerializeField]
+    private GameObject itemButton;
+    //　ゲーム再開ボタン
+    [SerializeField]
+    private GameObject reStartButton;
+    //　アイテムメニューパネル
+    [SerializeField]
+    private GameObject itemPanel;
+    // タイトルへのボタン
+    [SerializeField]
+    private GameObject Quit;
+
+    public void StopGame()
+    {
+        Time.timeScale = 0f;
+        itemButton.SetActive(false);
+        reStartButton.SetActive(true);
+        itemPanel.SetActive(true);
+        Quit.SetActive(true);
+    }
+
+    public void ReStartGame()
+    {
+        itemPanel.SetActive(false);
+        reStartButton.SetActive(false);
+        itemButton.SetActive(true);
+        Quit.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void ReturnTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
