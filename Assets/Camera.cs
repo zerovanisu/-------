b55cda948a18ea38a8425abdacb1a3b7b638c051
miku <mikudragonred@gmail.com>
2020-07-30 21:56:57 +0900

    miku1

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -16,7 +16,7 @@ public class Camera : MonoBehaviour
         Vector3 BellePos = Belle.transform.position;
 
         //カメラとプレイヤーの位置を同じにする
-        transform.position = new Vector3(BellePos.x, -1.7f, -4);
+        transform.position = new Vector3(BellePos.x, -2, -10);
     }
 
 }
